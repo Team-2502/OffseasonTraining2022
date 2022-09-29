347ebed1a0e5417753d4bb7db7acfa0bbfe74f95
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2022-09-29 18:34:15 -0500

    added conditionals

--- rust/primitives/src/main.rs
+++ rust/primitives/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let logical: bool = true;
 
-    let a_float: f64 = 1.0;
-    let an_integer   = 5i32;
+    let a_float: f32 = 1.0;
+    let an_integer: i64   = 5;
 
     let default_float   = 3.0;
     let default_integer = 7;
@@ -15,5 +15,34 @@ fn main() {
     
     //mutable = true;
     
-    let mutable = true;
+	let context_result: bool = {
+		let mutable = true;
+		mutable
+	};
+
+	println!("{}",context_result);
+
+	let mut context_result: i32 = if context_result {
+		println!("mutable was true!");
+		7
+	} else {
+		println!("mutable was false!");
+		4
+	};
+
+	context_result = 222;
+
+	println!("{}", context_result);
+
+	match context_result {
+		1 =>  println!("one"),
+		7 | 4 | 3 | 2 => println!("good number"),
+		s => {
+			println!("nonstandard number");
+			if (s % 2 == 0) {
+				println!("even number");
+			}
+		}
+	}
+
 }
