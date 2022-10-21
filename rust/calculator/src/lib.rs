7c4c4dbd701c8da43a4de57ed92196f046f94d1e
LisaT2005 <lisathayil@gmail.com>
2022-10-20 19:00:13 -0500

    sqrt

---
+++ rust/calculator/src/lib.rs
@@ -0,0 +1,65 @@
+use std::error::Error;
+use std::io;
+mod components;
+
+use crate::components::add;
+use crate::components::sqrt;
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+
+    let selectedMode: mode = getMode().unwrap();
+    println!("{:?}",selectedMode);
+
+    match selectedMode {
+        mode::ADD => {
+            println!("{}",add::add(1,2));
+        },
+        mode::SQRT => {
+            println!("Which number");
+            let mut a: f64 = 1.0;
+            let mut str = String::new();
+            io::stdin().read_line(&mut str);
+            a = str.trim().parse()?;
+
+            println!("{}", sqrt::sqrt(a));
+        },
+        _ => {
+            println!("Unimplemented");
+        }
+        
+    }
+    
+
+    Ok(())
+}
+
+#[derive(Debug)]
+enum mode {
+    FIB,
+    ADD,
+    SUB,
+    FIZZ,
+    SQRT,
+    UNKNOWN,
+}
+
+fn getMode() -> Result<mode, Box<dyn Error>> {
+    println!("What mode do you want?");
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+
+    let id: u16 = buffer.trim().parse()?;
+    //let id = buffer.trim.parse::<i32>().unwrap();
+
+
+    match id {
+        1 => Ok(mode::FIB),
+        2 => Ok(mode::ADD),
+        3 => Ok(mode::SUB),
+        5 => Ok(mode::SQRT),
+        _ => Ok(mode::UNKNOWN),
+    }
+
+}
