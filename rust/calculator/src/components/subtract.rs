f83e0e9be55bd7ef137793b8946cec1098be5bb9
Nytroid <98678068+Nytroid@users.noreply.github.com>
2022-11-03 18:11:40 -0500

    subtract

--- rust/calculator/src/components/subtract.rs
+++ rust/calculator/src/components/subtract.rs
@@ -1,3 +1,14 @@
-pub fn subtract(a: i32, b: i32) -> i32 {
-    a - b
+use crate::Component;
+pub struct subtract;
+impl Component for subtract {
+    fn new() -> subtract {
+        subtract{}
+    }
+    fn run(&self) -> f64 {
+        println!("input the first number:");
+        let a = self.input();
+        println!("input the second number");
+        let b = self.input();
+        a-b    
+    }
 }
