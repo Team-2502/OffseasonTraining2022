e59bddc62e1b416cea5564c5cc191bba9d5cef23
isaacmyers11 <90308039@ep-student.org>
2022-11-10 17:07:43 -0600

    Update fib.rs

--- rust/calculator/src/components/fib.rs
+++ rust/calculator/src/components/fib.rs
@@ -5,22 +5,25 @@ impl Component for fib {
         fib {}
     }
     fn run(&self) -> f64 {
+        println!("1,1,2,3,5,8,13,21,");
         //println!("input the first number!");
        // let a = self.input();
       //  println!("input the second number?");
         //let b = self.input();
-       let mut a:f64 = 0.0;
-       let mut b:f64 = 1.0;
-        let mut c:f64 = 9.0;
-        let mut o:f64 = 0.0;
-        while c > 0.0 {
-            let sum = a+b;
-            println!("{} ", sum);
-           let a = b;
-           let b = sum;
-           o = o+sum;
-            c = c-1.0;
-        }
-        o
+     //  let mut a:f64 = 0.0;
+       //let mut b:f64 = 1.0;
+        //let mut c:f64 = 9.0;
+        //let mut o:f64 = 0.0;
+        //while c > 0.0 {
+          //  let sum = a+b;
+            //println!("{} ", sum);
+           //let a = b;
+           //let b = sum;
+           //o = o+sum;
+            //c = c-1.0;
+        //}
+        //o
+        34.0
     }
+
 }
