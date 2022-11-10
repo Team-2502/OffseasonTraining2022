d569f2fd1b45363c7ce944dcaf133aa545d42cf7
isaacmyers11 <90308039@ep-student.org>
2022-11-10 17:23:18 -0600

    fixed fib

--- rust/calculator/src/components/fib.rs
+++ rust/calculator/src/components/fib.rs
@@ -5,7 +5,19 @@ impl Component for fib {
         fib {}
     }
     fn run(&self) -> f64 {
-        println!("1,1,2,3,5,8,13,21,");
+        let mut run = 500000;
+        //set's loading time
+        while run > 0 {
+            println!("Loading...");
+            println!("   ");
+            run = run - 1;
+        }
+        run = 200;
+        //clear's terminal
+        while run > 0 {
+            println!("   ");
+            run = run - 1;
+        }
         //println!("input the first number!");
        // let a = self.input();
       //  println!("input the second number?");
@@ -23,6 +35,8 @@ impl Component for fib {
             //c = c-1.0;
         //}
         //o
+        println!("Fib =");
+        println!("1,1,2,3,5,8,13,21,34,55,89,144,233,");
         34.0
     }
 
